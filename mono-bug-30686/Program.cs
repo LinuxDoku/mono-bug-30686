5c808e2cef56723c9b51ff418ad4a9b1684569ad
Martin <martin@linux-doku.de>
2016-03-21 20:38:48 +0100

    initial commit

---
+++ mono-bug-30686/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace monobug30686
+{
+	class MyFakeStream : FileStream 
+    {
+		public MyFakeStream (string path, FileMode mode) : base(path, mode) {}
+
+        /// <summary>
+        /// Simulate "CanSeek" is false, which is the case when you are retreiving data from web.
+        /// </summary>
+		public override bool CanSeek => false;
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var stream = new MyFakeStream("mono-bug-30686.zip", FileMode.Open);
+
+			var unzipped = new MemoryStream ();
+			var writer = new StreamWriter (unzipped);
+
+			using (var archive = new ZipArchive (stream, ZipArchiveMode.Read)) {
+				var entry = archive.Entries.First (x => x.Name == "Test.txt");
+				writer.Write (new StreamReader(entry.Open()).ReadToEnd());
+			    writer.Flush();
+			}
+
+			Console.WriteLine (Encoding.ASCII.GetString(unzipped.ToArray()));
+		    Console.ReadLine();
+		}
+	}
+}
